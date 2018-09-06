3a257268319c51cad52097b9e267be9d9b5c4123
Ariansyah Akbar <sevendifferenthero@gmail.com>
2018-09-06 16:08:59 +0700

    Perbaikan V.1

--- send.php
+++ send.php
@@ -1,28 +1,20 @@
 <?php
-$name=$_POST['nama'];
-$email=$_POST['email'];
-$subject=$_POST['telp'];
-$message=$_POST['pesan'];
-$to="akbatmetak@gmail.com";
-$message="Dear, <br /> <br />".$message;
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-// More headers
-$headers .= 'From:'.$name.' <'.$email.'>'."\r\n" . 'Reply-To: '.$name.' <'.$email.'>'."\r\n";
-$headers .= 'Cc: admin@email.com' . "\r\n"; //untuk cc lebih dari satu tinggal kasih koma
-@mail($to,$subject,$message,$headers);
-if (@mail) { ?>
-<script language="javascript" type="text/javascript">
-alert('Thank you for the message. We will contact you shortly.');
-window.location = 'index.html';
-</script>
-<?php
-}
-else { ?>
-<script language="javascript" type="text/javascript">
-alert('Message failed. Please, send an email to akbatmetal@gmail.com');
-window.location = 'index.html';
-</script>
-<?php
+if($_POST['kirim']){
+	$admin = 'akbatmetal@gmail.com'; //ganti email dg email admin (email penerima pesan)
+	
+	$nama	= htmlentities($_POST['nama']);
+	$email	= htmlentities($_POST['email']);
+	$judul	= htmlentities($_POST['telp']);
+	$pesan	= htmlentities($_POST['pesan']);
+	
+	$pengirim	= 'Dari: '.$nama.' <'.$email.'>';
+	
+	if(mail($admin, $judul, $pesan, $pengirim)){
+		echo 'SUCCESS: Pesan anda berhasil di kirim. <a href="index.php">Kembali</a>';
+	}else{
+		echo 'ERROR: Pesan anda gagal di kirim silahkan coba lagi. <a href="index.php">Kembali</a>';
+	}
+}else{
+	header("Location: index.html");
 }
 ?>
